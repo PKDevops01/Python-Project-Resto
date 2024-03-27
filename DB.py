b6a8eb42eb708e0cadd742fc4ed3b81964235b81
Pranav Eknath kharche <138126296+PKDevops01@users.noreply.github.com>
2024-03-27 10:12:38 +0530

    added db file

---
+++ DB.py
@@ -0,0 +1,8 @@
+# import mysql.connector
+# mydb=mysql.connector.connect(host="localhost", user="root", passwd="Pranav@123##", database="Pranavdb")
+# mycursor=mydb.cursor()
+# '''mycursor.execute("Show databases")'''
+# mycursor.execute("Drop Table RestoM")
+
+'''mycursor.execute("Create table RestoM(SNo int(3),Customer_Name varchar(200),Amount double,BillNo int(6) )")'''
+#mucursor.execute("Alter Table Resto")
